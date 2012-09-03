4acdd19284d7ed1f433f749470fc715c03f13ac5
mhebing <mhebing@diw.de>
2012-09-03 10:05:42 +0200

    Initial commit (including basic.sh)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DdiOnRails::Application.config.secret_token = 'c38e1a87983b9325a3ec8dc4fb1518f1fb7beaa917dc2a428c0336107552c24a6a76722c170f93aa83511a6e603d95f1326796a531b9a43944332b0b3c4afb3f'
