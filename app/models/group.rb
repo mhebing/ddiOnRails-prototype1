6fe50ba31e7c063cd746576c13a312d64f0e49a0
mhebing <mhebing@diw.de>
2012-09-07 19:21:57 +0200

    Group import

--- app/models/group.rb
+++ app/models/group.rb
@@ -20,4 +20,32 @@ class Group < ActiveRecord::Base
   def title
     if label.blank? then name else label end
   end
+
+  def import(filename = "import/group.xml")
+    file = File.open(filename)
+    group = Nokogiri::XML(file)
+    file.close
+
+    group.xpath("//var").each do |var|
+      if var.xpath("@files").blank?
+        then @physical_data_product = PhysicalDataProduct.find_or_create_by_name_and_group_id("default", id)
+        else @physical_data_product = PhysicalDataProduct.find_or_create_by_name_and_group_id(var.xpath("@files"), id)
+      end
+      @variable = Variable.new
+      @variable.name = var.xpath("@name").to_s unless var.xpath("@name").blank?
+      @variable.label = var.xpath(".//labl").children.to_s unless var.xpath(".//labl").blank?
+      @variable.physical_data_product = @physical_data_product
+      var.xpath(".//sumStat").each do |sumStat|
+        @variable.variable_statistics << VariableStatistic.new(
+           statistic: sumStat.xpath("@type").to_s,
+           value: sumStat.children.to_s)
+      end
+      var.xpath(".//catgry").each do |catgry|
+        @variable.variable_categories << VariableCategory.new(
+           label: catgry.xpath(".//catValu").children.to_s,
+           frequency: catgry.xpath(".//catStat").children.to_s)
+      end
+      @variable.save
+    end
+  end
 end
