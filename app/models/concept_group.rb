3551bb8ee6e4af1dccf288a7ef277f6690b2eb1c
mhebing <mhebing@diw.de>
2012-09-07 15:23:04 +0200

    Import for ConceptGroup and StudyUnit

--- app/models/concept_group.rb
+++ app/models/concept_group.rb
@@ -21,4 +21,19 @@ class ConceptGroup < ActiveRecord::Base
   def title
     if label.blank? then name else label end
   end
+
+  def self.import(filename="import/concept_groups.csv")
+    CSV.foreach(filename, :headers => true) do |row|
+      @row = row.to_hash
+      if !@row["parent"].blank? &
+         !ConceptGroup.find_by_name(@row["parent"]).blank?
+      then
+        @row["parent_id"] = ConceptGroup.find_by_name(@row["parent"]).id
+      else
+        @row["parent_id"] = nil
+      end
+      ConceptGroup.create(name: @row["name"],
+                          parent_id: @row["parent_id"])
+    end
+  end
 end
